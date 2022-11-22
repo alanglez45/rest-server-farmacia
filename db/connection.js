160b790a35410f83b8fe3f336695c1e4dcea7e03
alanglez45 <alanglezcorona@gmail.com>
2022-11-22 14:02:08 -0600

    commit

--- db/connection.js
+++ db/connection.js
@@ -5,10 +5,10 @@ const { Sequelize } = require('sequelize');
     host: 'localhost',
     dialect: 'mysql',
 }); */
-const db = new Sequelize(`${process.env.NODE_ENV_DB_NAME}`, `${process.env.NODE_ENV_DB_USER}`, `${process.env.NODE_ENV_DB_PASSWORD}`, {
-    host: `${process.env.NODE_ENV_DB_HOST}`,
+const db = new Sequelize(process.env.NODE_ENV_DB_NAME, process.env.NODE_ENV_DB_USER, process.env.NODE_ENV_DB_PASSWORD, {
+    host: process.env.NODE_ENV_DB_HOST,
     dialect: 'mysql',
-    port: `${process.env.NODE_ENV_DB_PORT}`
+    port: process.env.NODE_ENV_DB_PORT
 });
 
 module.exports = db;
